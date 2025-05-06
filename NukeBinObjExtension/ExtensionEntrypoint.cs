c4e17d714f5a01b43bd58e4a09d99ed9f1800d27
David Humphreys <newky2k@users.noreply.github.com>
2025-05-06 16:27:53 +0100

    Added initial version of the extension

---
+++ NukeBinObjExtension/ExtensionEntrypoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace NukeBinObjExtension
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class ExtensionEntrypoint : Extension
+    {
+        /// <inheritdoc/>
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            Metadata = new(
+                    id: "NukeBinObjExtension.86019190-4440-4de9-bf7e-590dd3d2c64b",
+                    version: this.ExtensionAssemblyVersion,
+                    publisherName: "Newky2k",
+                    displayName: "Nuke Bin/Obj",
+                    description: "Simple extension to fully delete the bin/obj folder of the selected project"),
+        };
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+
+            // You can configure dependency injection here by adding services to the serviceCollection.
+        }
+    }
+}
---
+++ NukeBinObjExtension/NukeCommand.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics;
+using Microsoft;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Shell;
+using Microsoft.VisualStudio.ProjectSystem.Query;
+
+namespace NukeBinObjExtension
+{
+    /// <summary>
+    /// Command1 handler.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class NukeCommand : Command
+    {
+        private readonly TraceSource logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NukeCommand"/> class.
+        /// </summary>
+        /// <param name="traceSource">Trace source instance to utilize.</param>
+        public NukeCommand(TraceSource traceSource)
+        {
+            // This optional TraceSource can be used for logging in the command. You can use dependency injection to access
+            // other services here as well.
+            this.logger = Requires.NotNull(traceSource, nameof(traceSource));
+        }
+
+        /// <inheritdoc />
+        public override CommandConfiguration CommandConfiguration => new("%NukeBinObjExtension.NukeCommand.DisplayName%")
+        {
+            // Use this object initializer to set optional parameters for the command. The required parameter,
+            // displayName, is set above. DisplayName is localized and references an entry in .vsextension\string-resources.json.
+            Icon = new(ImageMoniker.KnownValues.CleanData, IconSettings.IconAndText),
+            Placements =
+            [
+                // Project context menu
+                CommandPlacement.VsctParent(new Guid("{d309f791-903f-11d0-9efc-00a0c911004f}"), id: 518, priority: 0),
+            ],
+        };
+
+        /// <inheritdoc />
+        public override Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            // Use InitializeAsync for any one-time setup or initialization.
+            return base.InitializeAsync(cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+        {
+#pragma warning disable VSEXTPREVIEW_PROJECTQUERY_PROPERTIES_BUILDPROPERTIES // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+            var project = await context.GetActiveProjectAsync(project => project.With(p => new { p.Name, p.Path })
+                            .With(p => p.ActiveConfigurations),
+                            cancellationToken);
+#pragma warning restore VSEXTPREVIEW_PROJECTQUERY_PROPERTIES_BUILDPROPERTIES // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+            if (project != null)
+            {
+                var projectName = project.Name;
+                var projectPath = project.Path;
+                var directory = Path.GetDirectoryName(projectPath);
+
+                if (directory == null)
+                {
+                    return;
+                }
+
+                var result = await this.Extensibility.Shell().ShowPromptAsync($"Are you sure you want to delete the bin/obj folders for {projectName} ?", PromptOptions.OKCancel, cancellationToken);
+
+                if (result)
+                {
+                    var binFolder = Path.Combine(directory, "bin");
+
+                    if (Directory.Exists(binFolder))
+                    {
+                        Directory.Delete(binFolder, true);
+                    }
+
+                    var objFolder = Path.Combine(directory, "obj");
+
+                    if (Directory.Exists(objFolder))
+                    {
+                        Directory.Delete(objFolder, true);
+                    }
+
+                    await this.Extensibility.Shell().ShowPromptAsync("The bin/obj folders have been Nuked!", PromptOptions.OK, cancellationToken);
+                }
+            }
+           
+           
+        }
+    }
+}
